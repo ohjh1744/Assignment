635f812f6f67fc1ce0bd290fb76bd5be84a643e3
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-07-22 16:26:43 +0900

    Update Class Assignment
    Class 기본실습과 심화실습 push

---
+++ Class240722/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.InteropServices;
+
+namespace StudyClass
+{
+    class Chracter
+    {
+        protected int level;
+        protected float hp = 100;
+        protected float speed;
+        protected float damage;
+
+        public void ForWard()
+        {
+            Console.WriteLine("전진!");
+        }
+
+        public void BackWard()
+        {
+            Console.WriteLine("후진!");
+        }
+
+        public void Left()
+        {
+            Console.WriteLine("좌회전(90도)!");
+        }
+
+        public void Right()
+        {
+            Console.WriteLine("우회전(90도)!");
+        }
+        public void Attack()
+        {
+            Console.WriteLine("공격!");
+        }
+
+        public void GetDamage()
+        {
+            hp -= 10;
+            Console.WriteLine("피격!");
+        }
+
+    }
+    
+    internal class Program
+    {
+       
+    }
+}
