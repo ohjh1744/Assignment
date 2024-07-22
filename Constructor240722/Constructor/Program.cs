7bf300e26d736ed67c70f640a57271466e420ec8
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-07-22 19:57:23 +0900

    생성자 과제 Update
    생성자 과제 기본+심화 Update
    심화: 가비지 컬렉터, 소멸자 관련문제

---
+++ Constructor240722/Constructor/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Numerics;
+
+namespace Constructor
+{
+    internal class Program
+    {
+        class Monster
+        {
+            private int hp;
+
+            public Monster(int hp)
+            {
+                this.hp = hp;
+            }
+            public int ShowHp
+            {
+                get { return hp; }
+            }
+
+        }
+        class Trainer
+        {
+            private string name;
+            private List<Monster> monsters;
+
+            public Trainer(string name)
+            {
+                this.name = name;
+                this.monsters = new List<Monster>();
+            }
+
+            public void CreateMonster(int hp)
+            {
+                monsters.Add(new Monster(hp));
+            }
+
+            public void ShowInformation()
+            {
+                Console.WriteLine($"트레이너 이름은: {name}");
+
+                for (int i = 0; i < monsters.Count; i++)
+                {
+                    Console.WriteLine($"{i}번째 몬스터의 체력: {monsters[i].ShowHp}");
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            Trainer trainer = new Trainer("오준혁");
+            trainer.CreateMonster(50);
+            trainer.CreateMonster(60);
+            trainer.ShowInformation();
+        }
+
+    }
+
+
+}
