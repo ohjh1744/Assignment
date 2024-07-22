f5c70599bdcdaaeff0a185b282af41f1afad63c6
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-07-22 16:53:16 +0900

    Static assginment update
    Static 기본 및 심화실습 push

---
+++ Static240722/Static기본실습/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Static
+{
+    internal class Program
+    {
+        class MyCalculator
+        {
+            public double Add(double x, double y)
+            {
+                return x + y;
+            }
+            public double Subtract(double x, double y)
+            {
+                return x - y;
+            }
+            public double Multiply(double x, double y)
+            {
+                return x * y;
+            }
+            public double Divide(double x, double y)
+            {
+                if(y == 0)
+                {
+                    Console.WriteLine("0으로 나누어 에러 발생");
+                    return 0;
+                }
+                return x / y;
+            }
+            public double Squard(double x, double y)
+            {
+                return (double)MathF.Pow((float)x , (float)y);
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
