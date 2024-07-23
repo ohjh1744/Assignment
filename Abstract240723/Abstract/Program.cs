52ca9ead82e8cb3d0122852598abb41f07b9e7a6
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-07-23 19:07:09 +0900

    추상클래스 과제 제출
    추상클래스 과제 제출

---
+++ Abstract240723/Abstract/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace Abstract
+{
+    internal class Program
+    {
+
+
+        public abstract class LolChampion
+        {
+            protected string name;
+            protected int hp;
+            protected int level;
+            public abstract void SkillQ();
+
+            public abstract void SkillW();
+
+            public abstract void SkillE();
+
+        }
+
+        public class Garen : LolChampion
+        {
+            public override void SkillQ()
+            {
+                Console.WriteLine(("결정타"));
+            }
+            public override void SkillW()
+            {
+                Console.WriteLine(("용기"));
+            }
+            public override void SkillE()
+            {
+                Console.WriteLine(("심판"));
+            }
+        }
+
+        public class Graves : LolChampion
+        {
+            public override void SkillQ()
+            {
+                Console.WriteLine(("화약 역류"));
+            }
+            public override void SkillW()
+            {
+                Console.WriteLine(("연막탄"));
+            }
+            public override void SkillE()
+            {
+                Console.WriteLine(("빨리 뽑기"));
+            }
+        }
+
+        public class Gnar : LolChampion
+        {
+            public override void SkillQ()
+            {
+                Console.WriteLine(("부메랑 던지기"));
+            }
+            public override void SkillW()
+            {
+                Console.WriteLine(("슝슝"));
+            }
+            public override void SkillE()
+            {
+                Console.WriteLine(("폴짝"));
+            }
+        }
+
+        //과제 2
+        // 추상클래스: 직접적으로 인스턴스를 생성 할 수 없는 클래스로, 다른 클래스들이 주로 상속받아 사용하도록 하는 클래스
+        // 오버라이딩:상속관계에서 부모 클래스의 메서드를 자식 클래스에서 재정의하는 다형성 방법중 하나
+        // 오버로딩: 메서드 명은 동일하지만, 매개변수와 반환타을 다르게한 다형성 방법중 하나
+        static void Main(string[] args)
+        {
+           
+
+        }
+    }
+}
---
+++ Abstract240723/AbstractClass2/Program.cs
@@ -0,0 +1,122 @@
+﻿using static AbstractClass2.Program;
+using System.Reflection.Emit;
+using System.Xml.Linq;
+
+namespace AbstractClass2
+{
+    internal class Program
+    {
+        public enum MobType { Electric, Fire, Water, Grass , None}
+        public abstract class Monster
+        {
+           
+            private int level;
+            private MobType mobtype = MobType.Electric;
+            private string name;
+            protected Monster(string name, int level, MobType mobtype)
+            {
+                this.name = name;
+                this.level = level;
+                this.mobtype = mobtype;
+            }
+            public  abstract void BaseAttack();
+
+            public int getLevel{ get { return level; }  }
+            public MobType getType { get { return  mobtype; } }
+            public string getName { get { return name; } }
+        }
+
+        public class Pikachu: Monster
+        {
+            public Pikachu(string name, int level, MobType mobtype) : base(name, level, mobtype) { }
+      
+            public override void BaseAttack()
+            {
+                Console.WriteLine("전광석화");
+            }
+        }
+        public class Squirtle : Monster
+        {
+            public Squirtle(string name, int level, MobType mobtype) : base(name, level, mobtype) { }
+            public override void BaseAttack()
+            {
+                Console.WriteLine("물총발사");
+            }
+        }
+        public class Bulbasaur : Monster
+        {
+            public Bulbasaur(string name, int level, MobType mobtype) : base(name, level, mobtype) { }
+
+            public override void BaseAttack()
+            {
+                Console.WriteLine("덩굴채찍");
+            }
+        }
+        public class Charmander : Monster
+        {
+            public Charmander(string name, int level, MobType mobtype) : base(name, level, mobtype) { }
+            public override void BaseAttack()
+            {
+                Console.WriteLine("화염방사");
+            }
+        }
+
+        public class Trainer
+        {
+            Monster[] monsters = new Monster[6];
+           
+            
+            public Trainer()
+            {
+                for(int i = 0; i <6; i++)
+                {
+                    monsters[i] = new Pikachu("없음", 0, MobType.None);
+                }
+                monsters[0] = new Pikachu("Pikachu", 3, MobType.Electric);
+            }
+
+            public void CreateMonster(int num , string name, int level, MobType mobtype)
+            {
+                switch (name)
+                {
+                    case "Pikachu":
+                        monsters[num] = new Pikachu(name, level, mobtype);
+                        break;
+                    case "Squirtle":
+                        monsters[num] = new Squirtle(name, level, mobtype);
+                        break;
+                    case "Bulbasaur":
+                        monsters[num] = new Bulbasaur(name, level, mobtype);
+                        break;
+                    case "Charmander":
+                        monsters[num] = new Charmander(name, level, mobtype);
+                        break;
+                }
+                 
+            }
+
+            public void AllAttack()
+            {
+                foreach(Monster monster in monsters)
+                {
+                    if(monster.getType == MobType.None)
+                    {
+                        continue;
+                    }
+                    monster.BaseAttack();
+                }
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Trainer trainer = new Trainer();
+            trainer.CreateMonster(1, "Charmander", 5, MobType.Fire);
+            trainer.AllAttack();
+        
+
+        }
+    }
+}
