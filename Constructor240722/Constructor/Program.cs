786c81528f0e908c337f1d8adb8595cf40e83471
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-07-25 23:08:18 +0900

    추상클래스 생성자 과제 수정
    추상클래스 생성자 과제 수정

--- Constructor240722/Constructor/Program.cs
+++ Constructor240722/Constructor/Program.cs
@@ -1,47 +1,78 @@
-﻿using System.Numerics;
-
-namespace Constructor
+﻿namespace Constructor
 {
     internal class Program
     {
 
+        //  이름과 체력을 기본적으로 갖고있는 포켓몬을 잡으면
+        // Trainer가 본인의 Monster배열안에 저장하는식으로 재구성해서 구현.
         public abstract class Monster
         {
-            protected Tuple<string, int>[] monsterStatus;
-            public abstract void CreateMonster(int num, string name, int hp);
-            public abstract void ShowMonster(int num);
+            private int hp;
+            private string name;
+
+            protected Monster(string name, int hp)
+            {
+                this.name = name;
+                this.hp = hp;
+            }
+         
+            public int Hp
+            {
+                get { return hp; }
+                set { hp = value; }
+            }
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
 
         }
 
-        public class Trainer : Monster
+        public class Poketmon : Monster
+        {
+            public Poketmon(string name, int hp) : base(name, hp) { }
+
+        }
+
+        public class Trainer
         {
             private string name;
+            private Monster[] monsters = new Monster[6];
+            private int numHave = 0;
 
             public Trainer(string name)
             {
                 this.name = name;
-                base.monsterStatus = new Tuple<string, int>[6];
             }
-            public override void CreateMonster(int num, string name, int hp)
+
+            public void TakeMonster(Monster monster)
             {
-                monsterStatus[num] = new Tuple<string, int>(name, hp);
+                if (numHave >= 6)
+                {
+                    Console.WriteLine("포켓몬을 더이상 가질 수 없습니다.");
+                }
+                monsters[numHave++] = monster;
             }
-            public override void ShowMonster(int num)
+
+            public void ShowInformation()
             {
-                Console.WriteLine($"{num}번째 몬스터이름: {monsterStatus[num].Item1} ");
-                Console.WriteLine($"{num}번째 체력: {monsterStatus[num].Item2} ");
+                Console.WriteLine($"제 이름은 {name}입니다.");
+                for (int i = 0; i < numHave; i++)
+                {
+                    Console.WriteLine($"{i}번째 포켓몬: {monsters[i].Name}");
+                }
             }
 
         }
+
         static void Main(string[] args)
         {
 
             Trainer trainer = new Trainer("오준혁");
-            trainer.CreateMonster(1, "피카츄", 50);
-            trainer.CreateMonster(2, "가라도스", 60);
-            trainer.ShowMonster(1);
-            trainer.ShowMonster(2);
-
+            Monster pikachu = new Poketmon("피카츄", 100);
+            trainer.TakeMonster(pikachu);
+            trainer.ShowInformation();
         }
 
     }
