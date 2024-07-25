60cd20dd5393afa43457078733bdec7aed2cd2a3
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-07-25 23:21:09 +0900

    생성자과제 수정
    생성자 과제 수정

--- Constructor240722/Constructor/Program.cs
+++ Constructor240722/Constructor/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Constructor
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Constructor
 {
     internal class Program
     {
 
         //  이름과 체력을 기본적으로 갖고있는 포켓몬을 잡으면
         // Trainer가 본인의 Monster배열안에 저장하는식으로 재구성해서 구현.
+        // Trainer의 UpdateMonster에서 포켓몬의 속성바꾸기 가능.
         public abstract class Monster
         {
             private int hp;
@@ -50,9 +53,26 @@
             {
                 if (numHave >= 6)
                 {
-                    Console.WriteLine("포켓몬을 더이상 가질 수 없습니다.");
+                    Console.WriteLine("포켓몬을 더이상 가질 수 없습니다.");    
                 }
-                monsters[numHave++] = monster;
+                else
+                {
+                    monsters[numHave++] = monster;
+                }           
+            }
+
+            public void UpdateMonster(int monsterNum , string name, int hp)
+            {
+                if(monsterNum > numHave)
+                {
+                    Console.WriteLine("잘못된 포켓몬 숫자입니다.");
+                }
+                else
+                {
+                    monsters[monsterNum-1].Name = name;
+                    monsters[monsterNum-1].Hp = hp;
+                }
+               
             }
 
             public void ShowInformation()
@@ -72,6 +92,8 @@
             Trainer trainer = new Trainer("오준혁");
             Monster pikachu = new Poketmon("피카츄", 100);
             trainer.TakeMonster(pikachu);
+            trainer.ShowInformation();
+            trainer.UpdateMonster(1, "거북몬", 50);
             trainer.ShowInformation();
         }
 
