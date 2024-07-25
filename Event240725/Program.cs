5330fab1c228c1949f2f748dbc28e1408d484f75
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-07-25 21:34:30 +0900

    과제올림
    과제올림

---
+++ Event240725/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Event
+{
+
+    public class Program
+    {
+        public class Player
+        {
+            private Armor curArmor;
+
+            public void Equip(Armor armor)
+            {
+                Console.WriteLine($"플레이어가 {armor.name} 을/를 착용합니다.");
+                curArmor = armor;
+                curArmor.OnBreaked += UnEquip;
+            }
+
+            public void UnEquip()
+            {
+                Console.WriteLine($"플레이어가 {curArmor.name} 을/를 해제합니다.");
+                curArmor.OnBreaked -= UnEquip;
+                curArmor = null;
+            }
+
+            public void Hit()
+            {
+                Console.WriteLine("플레이어가 피해를 받는다.");
+                curArmor.DecreaseDurability();
+            }
+        }
+
+        public class Armor
+        {
+            public string name;
+            private int durability;
+
+            public event Action OnBreaked;
+
+            public Armor(string name, int durability)
+            {
+                this.name = name;
+                this.durability = durability;
+            }
+
+            public void DecreaseDurability()
+            {
+                durability--;
+                if (durability <= 0)
+                {
+                    Break();
+                }
+            }
+
+            private void Break()
+            {
+                Console.WriteLine("방어구가 부서졌습니다");
+                OnBreaked?.Invoke();
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            Armor ammor = new Armor("갑옷", 3);
+            player.Equip(ammor);
+
+            player.Hit();
+            player.Hit();
+            player.Hit();
+        }
+    }
+
+}
---
+++ ExtendMethod0725/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ExtendMethod0725
+{
+    
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("아이디를 입력하세요 : ");
+            string id = Console.ReadLine();
+
+            if (id.IsAllowedID())
+            {
+                Console.WriteLine("ID가 유효합니다.");
+            }
+            else
+            {
+                Console.WriteLine("ID에 허용되지 않는 특수문자가 있습니다.");
+            }
+        }    
+    }
+    public static class Extended
+    {
+        public static bool IsAllowedID(this string id)
+        {
+            for (int i = 0; i < id.Length; i++)
+            {
+                if (!char.IsLetterOrDigit(id[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+
+}
