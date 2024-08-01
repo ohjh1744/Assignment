776265947ce657ac9408335fde3745e7b9fc5d35
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-08-01 11:26:50 +0900

    스택큐과제2 수정
    스택큐과제2 수정

--- StackQueue0726/기본실습/과제2/Program.cs
+++ StackQueue0726/기본실습/과제2/Program.cs
@@ -6,56 +6,53 @@
         {
             int n;
             int day = 1;
-            int[] temp;
-       
-            //작업배열 수 입력
-            int.TryParse(Console.ReadLine(), out n);
-            temp = new int[n];
-
-            //각 작업의 걸리는 시간 입력
-            for(int i = 0; i < n; i++)
-            {
-                int.TryParse(Console.ReadLine(), out temp[i]);
-            }
-
-
-            int num = 0;
-            int curtime = 0;
-            int nexttime = 0;
+            int remainTime = 8;
             Queue<int> queue = new Queue<int>();
+            int[] result;
+            int index = 0;
 
-            while (num < n)
+            // 작업배열 수 입력
+            int.TryParse(Console.ReadLine(), out n);
+            result = new int[n];
+
+            // 각 작업의 걸리는 시간 입력
+            for (int i = 0; i < n; i++)
             {
-                int work = temp[num];
-                curtime = nexttime;
-                nexttime = curtime + work;
-
-                if (nexttime < 8)
-                {
-                    queue.Enqueue(day);
-                    num++;
-                }
-                else if(nexttime == 8)
-                {
-                    queue.Enqueue(day);
-                    num++;
-                    nexttime = 0;
-                    day++;
-                }
-                else if(nexttime > 8)
-                {
-                    day++;
-                    temp[num] = work - (8-curtime);
-                    nexttime = 0;
-                }
+                int work = int.Parse(Console.ReadLine());
+                queue.Enqueue(work);
             }
-           
 
+            // 작업 처리
             while (queue.Count > 0)
             {
-                Console.Write($"{queue.Dequeue()}");
+                int work = queue.Dequeue();
+
+                while (remainTime < work)
+                {
+                    work -= remainTime;
+                    day++;
+                    remainTime = 8;
+                }
+
+                result[index++] = day;
+                remainTime -= work;
+
+                // 작업시간이 8시간 딱떨어지면 초기화
+                if (remainTime == 0)
+                {
+                    day++;
+                    remainTime = 8;
+                }
             }
 
+            // 결과 출력
+            foreach(int i in result)
+            {
+                Console.Write(i);
+                Console.Write(" ");
+            }
+
+           
         }
     }
 }
