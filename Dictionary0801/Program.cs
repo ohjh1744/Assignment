a78bedd7b01b424af01f513191c481b1f8cf0525
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-08-05 01:14:24 +0900

    dictionary 과제 피드백수용후 업뎃

--- Dictionary0801/Program.cs
+++ Dictionary0801/Program.cs
@@ -2,52 +2,57 @@
 {
     internal class Program
     {
-        public static Dictionary<string, int> dict = new Dictionary<string, int>();
+        public static Dictionary<string, Monster> dict;
         public class MonsterData
         {
-            public MonsterData(string name, int level)
+            public static void Initialize()
             {
-                dict.Add(name, level);
+                dict = new Dictionary<string, Monster>();
+                dict.Add("피카츄", new Monster("피카츄"));
+                dict.Add("라이츄", new Monster("라이츄"));
+                dict.Add("파이리", new Monster("파이리"));
+                dict.Add("꼬부기", new Monster("꼬부기"));
+                dict.Add("가랴도스", new Monster("가랴도스"));
             }
+               
 
         }
         public class Monster
         {
             private string name;
-            private int level;
             public Monster(string name)
             {
                 this.name = name;
-                dict.TryGetValue(name, out level);
             }
 
             public string GetName { get { return name; } }
-            public int GetLevel
-            {
-                get { return level; }
-            }
         }
         static void Main(string[] args)
         {
 
-            MonsterData monsterData;
-            monsterData = new MonsterData("피카츄", 1);
-            monsterData = new MonsterData("라이츄", 2);
-            monsterData = new MonsterData("파이리", 3);
-            monsterData = new MonsterData("꼬부기", 4);
-            monsterData = new MonsterData("가랴도스", 5);
+            MonsterData.Initialize();
 
             Monster[] monsters = new Monster[5];
 
-            monsters[0] = new Monster("피카츄");
-            monsters[1] = new Monster("라이츄");
-            monsters[2] = new Monster("파이리");
-            monsters[3] = new Monster("꼬부기");
-            monsters[4] = new Monster("가랴도스");
+            Monster dicmonster;
+            dict.TryGetValue("피카츄", out dicmonster);
+            monsters[0] = new Monster(dicmonster.GetName);
+
+            dict.TryGetValue("라이츄", out dicmonster);
+            monsters[1] = new Monster(dicmonster.GetName);
+
+            dict.TryGetValue("파이리", out dicmonster);
+            monsters[2] = new Monster(dicmonster.GetName);
+
+            dict.TryGetValue("꼬부기", out dicmonster);
+            monsters[3] = new Monster(dicmonster.GetName);
+
+            dict.TryGetValue("가랴도스", out dicmonster);
+            monsters[4] = new Monster(dicmonster.GetName);
 
             foreach (Monster monster in monsters)
             {
-                Console.WriteLine($"몬스터이름: {monster.GetName} 몬스터레벨:{monster.GetLevel}");
+                Console.WriteLine($"몬스터이름: {monster.GetName}");
             }
 
 
